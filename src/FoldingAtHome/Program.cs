90066f5a30ec8b5fc1f33d9ad12db18a4a4ba36d
Rickard Löfberg <lofberg.rickard@gmail.com>
2022-09-09 11:28:38 +0200

    remove duplicate

--- src/FoldingAtHome/Program.cs
+++ src/FoldingAtHome/Program.cs
@@ -18,17 +18,4 @@ builder.Services.AddScoped<ITeamHttpRepository, TeamHttpRepository>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(uriString: "https://cors-anywhere.herokuapp.com/https://api.foldingathome.org/") });
 
-
-builder.Services.AddScoped<DialogService>();
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<TooltipService>();
-builder.Services.AddScoped<ContextMenuService>();
-
-builder.Services.AddScoped<IProjectHttpRepository, ProjectHttpRepository>();
-builder.Services.AddScoped<ITeamHttpRepository, TeamHttpRepository>();
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(uriString: "https://api.foldingathome.org/") });
-
-
-
 await builder.Build().RunAsync();
