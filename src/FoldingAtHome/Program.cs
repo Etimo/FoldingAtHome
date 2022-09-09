94cdd28a96c91345872f1efe59c6518de5a7acd5
Morgan Cromell <morgan.cromell@etimo.se>
2022-09-09 10:45:46 +0200

    fix: cors ....

--- src/FoldingAtHome/Program.cs
+++ src/FoldingAtHome/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddScoped<ContextMenuService>();
 builder.Services.AddScoped<IProjectHttpRepository, ProjectHttpRepository>();
 builder.Services.AddScoped<ITeamHttpRepository, TeamHttpRepository>();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(uriString: "https://api.allorigins.win/get?url=https://api.foldingathome.org/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(uriString: "https://crossorigin.me/https://api.foldingathome.org/") });
 
 
 
